3179b632cdc495fa5d370f0e926e0f6b9055d7a1
averyniceday <avjwang@ucdavis.edu>
2016-05-05 14:54:03 -0700

    first test of ui.R

---
+++ app/RiceVis/ui.R
@@ -0,0 +1,45 @@
+library(shiny)
+
+#starts SHINY page
+shinyUI(fluidPage( 
+  
+  titlePanel("Rice Data Visualization"),
+  
+  # Description of application
+  helpText("The following application creates a scatterplot of user-chosen ",
+           "phenotypic traits. Users can also choose whether to color the traits by",
+           " either population or country of origin."),
+  
+  # phenotypic trait choice
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons("pheno", 
+                   "Choose a phenotype to plot",
+                   c("Plant.height",
+                     "Seed.length",
+                     "Seed.width",
+                     "Seed.volume")
+      )),
+    
+    sidebarPanel(
+      radioButtons("trait", 
+                     "Choose a trait to plot the phenotype against",
+                     c("Alu.Tol",
+                       "Amylose.content",
+                       "Blast.resistance")
+        )),
+  
+    sidebarPanel(
+      radioButtons("color", 
+                     "Choose how to color the plot:",
+                     c("'virginica'",
+                       "'sertosa'",
+                       "'versicolor'")
+        )),
+    
+      # Show a plot of the generated distribution
+      mainPanel(plotOutput("boxPlot"))
+    )
+  )
+)
+  
